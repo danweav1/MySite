1f2a8a597efc7029babe0a266015482543c4a9ca
danweaver12 <danweaver12@gmail.com>
2020-04-09 23:35:21 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-site';
+export class AppComponent implements OnInit {
+  title = 'Daniel Weaver Software Engineer';
+
+  public constructor(private titleService: Title)
+  {
+
+  }
+
+  ngOnInit() {
+    this.setTitle(this.title);
+  }
+
+  public setTitle(newTitle: string)
+  {
+    this.titleService.setTitle(newTitle);
+  }
 }
